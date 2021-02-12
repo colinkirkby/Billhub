fef7319a5a049793cb8a16263cf3762be40f3b55
gsheer <gurpreet_heer_2@sfu.ca>
2021-02-11 20:49:40 -0800

    commit

--- src/main/java/com/Spring/BillHub/BillHubApplication.java
+++ src/main/java/com/Spring/BillHub/BillHubApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BillHubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BillHubApplication.class, args);
 	}
