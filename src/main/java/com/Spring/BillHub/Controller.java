5f6e2cd731fd577191785304c1330299502c047c
gsheer <gurpreet_heer_2@sfu.ca>
2021-02-14 16:21:43 -0800

    updating Controller.java

--- src/main/java/com/Spring/BillHub/Controller.java
+++ src/main/java/com/Spring/BillHub/Controller.java
@@ -4,8 +4,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+@org.springframework.stereotype.Controller
 public class Controller {
-    @GetMapping("/welcome")
+    @GetMapping("")
     public String welcome(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "welcome";
