1956356caceb795f5d2e8db98630c9ac34dbb1e8
jla731 <jla731@sfu.ca>
2021-02-17 18:09:23 -0800

    Removed yarn.lock files and updated maps page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/pages/Dashboard';
 import Account from './components/pages/Account';
-// import Maps from './components/pages/Maps';
+import Maps from './components/pages/Maps';
 import NewEntry from './components/pages/NewEntry';
 import EntryList from './components/pages/EntryList';
 
@@ -15,7 +15,7 @@ function App() {
           <Route path = '/' exact component = {Dashboard} />
           <Route path = '/dashboard' component = {Dashboard} />
           <Route path = '/account' component = {Account} />
-          {/* <Route path = '/maps' component = {Maps} /> */}
+          <Route path = '/maps' component = {Maps} />
           <Route path = '/new-entry' component = {NewEntry} />
           <Route path = '/entries' component = {EntryList} />
         </Switch>
