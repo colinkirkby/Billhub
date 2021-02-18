a7a3b795ce4101545bf01cfa250e0166c3afa71f
gsheer <gurpreet_heer_2@sfu.ca>
2021-02-17 20:51:52 -0800

    updating postgreSQL connection for Heroku

--- src/main/java/com/Spring/BillHub/BillHubController.java
+++ src/main/java/com/Spring/BillHub/BillHubController.java
@@ -1,7 +1,5 @@
 package com.Spring.BillHub;
 
-import com.Spring.BillHub.model.User;
-import com.Spring.BillHub.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,22 +7,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BillHubController {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @GetMapping("/index")
-    public void test() {
-        User user = new User();
-        user.setId(1);
-        user.setFirstName("Name");
-        user.setLastName("Last");
-        userRepository.save(user);
-
-        System.out.println(userRepository.count());
-
-        for(User u : userRepository.findAll()) {
-            System.out.println(u.getFirstName());
-        }
-    }
 }
