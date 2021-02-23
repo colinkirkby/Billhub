006e80e3be6765e4e077542b081adc2b15128d6c
gsheer <gurpreet_heer_2@sfu.ca>
2021-02-23 15:46:34 -0800

    updating user, role, userRepo, securityConfig, controller, AuditModel, and google sign credentials

--- src/main/java/com/Spring/BillHub/BillHubController.java
+++ src/main/java/com/Spring/BillHub/BillHubController.java
@@ -1,10 +1,33 @@
 package com.Spring.BillHub;
 
+import com.Spring.BillHub.model.User;
+import com.Spring.BillHub.repo.UserRepo;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 
 
-@Controller
+@RestController
 public class BillHubController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/users")
+    public Page<User> getUsers(Pageable pageable) {
+        return userRepo.findAll(pageable);
+    }
+
+    @GetMapping("/loggedin")
+    public String googleSignInCheck() {
+        return "LOGGED IN WITH GMAIL";
+    }
 }
