c57f92833a2dfdf4c3142f1c60b24168d9bb97b8
Tianyu <feelingzty@gmail.com>
2021-02-23 22:41:47 -0800

    fixed registration backend, login almost done

--- src/main/Registration Login/Controller/UserController.java
+++ src/main/Registration Login/Controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,14 +24,15 @@ import net.java.springboot.model.Role;
 import net.java.springboot.model.User;
 import net.java.springboot.repository.UserRepository;
 import net.java.springboot.service.UserService;
+import net.java.springboot.service.UserServicelmpl;
 import net.java.springboot.web.dto.UserRegistrationDto;
 
 @RestController         //use @controller annotation to let spring know that is class is a controller to handle html request
-@RequestMapping    //send html request to this address
 public class UserController {
      @Autowired
 	private UserService userService;
-	private UserRepository userRepository;
+     @Autowired
+	private UserRepository repository;
 
 	//constructor
 	public UserController(UserService userService) {
@@ -43,23 +45,21 @@ public class UserController {
 		return new UserRegistrationDto();
 	}
 	
-	@RequestMapping(path="/registration", method=RequestMethod.POST)       //tells spring that this method will handle post registration request
-	public User registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+
+	@PostMapping("/registration")
+	public User registerUserAccount(@RequestBody UserRegistrationDto registrationDto) {
 		return userService.save(registrationDto);
-		//return "redirect:/registration?success";   //redirct to the registration page with success message, success is a flag
 	}
 	
-	@RequestMapping(path="/login",method=RequestMethod.POST)
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-		User user =userRepository.findByEmail(username);
-		if(user ==null) {
+		
+	@PostMapping("/login")
+    public User loadUserByUsername(@RequestBody String username) throws UsernameNotFoundException{
+		User user = repository.findByEmail(username); 
+		if(user == null) {
 			throw new UsernameNotFoundException("Invalid username or password");
 		}
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
-	}
-	
-	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
-		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+		return repository.findByEmail(username);
+		//return user.getFirstName();
 	}
 	
 }
