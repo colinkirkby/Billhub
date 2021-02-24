7a51d3b732ab45169cc1239b29abd83b67efe6a4
gsheer <gurpreet_heer_2@sfu.ca>
2021-02-23 21:52:30 -0800

    adding redirect from "login-gmail" to "login-gmail-successful" to BillHubController.java

--- src/main/java/com/Spring/BillHub/BillHubController.java
+++ src/main/java/com/Spring/BillHub/BillHubController.java
@@ -8,8 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
@@ -18,13 +19,14 @@ public class BillHubController {
     @Autowired
     private UserRepo userRepo;
 
-    @GetMapping("/users")
+    @GetMapping("/all-users")
     public Page<User> getUsers(Pageable pageable) {
         return userRepo.findAll(pageable);
     }
 
+
     // login with google, if new user, add to database
-    @GetMapping("/user")
+    @GetMapping("/login-gmail-successful")
     String login(@AuthenticationPrincipal OidcUser user) {
         String email = (String) user.getAttributes().get("email");
 
@@ -38,9 +40,15 @@ public class BillHubController {
             newUser.setAccountType(AccountType.GMAIL);
             userRepo.save(newUser);
 
-            return newUser.toString();
+            return "Hello, " + newUser.getFirstName() + ", welcome to BillHub!";
         }
 
-        return googleSignInCheck.toString();
+        return "Hello " + googleSignInCheck.getFirstName() + ", welcome to BillHub!";
+    }
+
+    @GetMapping("/login-gmail")
+    public ModelAndView redirectWithUsingRedirectPrefix(ModelMap model) {
+        model.addAttribute("attribute", "login-gmail");
+        return new ModelAndView("redirect:/login-gmail-successful", model);
     }
 }
