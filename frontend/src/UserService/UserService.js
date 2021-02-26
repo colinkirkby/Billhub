1685d107e136bce761337b889291008e435ede3c
Tianyu <feelingzty@gmail.com>
2021-02-25 22:06:59 -0800

    changed on registration-frontend

---
+++ frontend/src/UserService/UserService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const USER_API_BASE_URL = "http://localhost:8080/api/v1/registration";
+
+class UserService{
+    createUser(user){
+        return axios.post(USER_API_BASE_URL, user);
+    }
+}
+
+export default new UserService()
